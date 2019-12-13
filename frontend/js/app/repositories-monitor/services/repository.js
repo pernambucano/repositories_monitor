c4b271a7a21484927932d8bcc90c6df2152f7b82
paulo <paulo@paulos-MacBook-Pro-3.local>
2019-12-13 19:16:53 -0300

    :bug: repository service calling wrong url

--- frontend/js/app/repositories-monitor/services/repository.js
+++ frontend/js/app/repositories-monitor/services/repository.js
@@ -1,9 +1,8 @@
 import axios from 'axios';
-const baseUrl = 'http://localhost:8000/api';
 
 export const getRepository = (repositoryPath) => {
   const token = localStorage.getItem('api-token');
-  return axios(`${baseUrl}/commits?repository-name=${repositoryPath}`, {
+  return axios(`/commits?repository-name=${repositoryPath}`, {
     method: 'get',
     headers: {
       Authorization: `Token ${token}`,
