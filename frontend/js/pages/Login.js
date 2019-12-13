3c65733eafa8728e45a9ea05e36b0d82dcaa8b1c
paulo <paulo@paulos-MacBook-Pro-3.local>
2019-12-13 17:19:56 -0300

    :recycle: Login page

--- frontend/js/pages/Login.js
+++ frontend/js/pages/Login.js
@@ -1,16 +1,80 @@
 import React, { useCallback } from 'react';
-import { Link } from 'react-router-dom';
 import authService from '../app/repositories-monitor/services/auth';
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
+import { AwesomeButton, AwesomeButtonSocial } from 'react-awesome-button';
+import AwesomeButtonStyles from 'react-awesome-button/src/styles';
+//import 'react-awesome-button/dist/themes/theme-blue.css';
 
 const Login = () => {
   const handleLoginWithGithub = useCallback(async function() {
     authService.authLogin();
   }, []);
 
+  const useStyles = makeStyles((theme) => ({
+    root: {
+      height: '100vh',
+    },
+    image: {
+      backgroundImage: 'url(https://source.unsplash.com/user/thehmstravels)',
+      backgroundRepeat: 'no-repeat',
+      backgroundColor:
+        theme.palette.type === 'dark' ? theme.palette.grey[900] : theme.palette.grey[50],
+      backgroundSize: 'cover',
+      backgroundPosition: 'center',
+    },
+    paper: {
+      margin: theme.spacing(8, 4),
+      display: 'flex',
+      flexDirection: 'column',
+      alignItems: 'center',
+    },
+    avatar: {
+      margin: theme.spacing(1),
+      backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+      width: '100%', // Fix IE 11 issue.
+      marginTop: theme.spacing(1),
+    },
+    submit: {
+      margin: theme.spacing(3, 0, 2),
+    },
+  }));
+
+  const classes = useStyles();
   return (
-    <div>
-      <button onClick={handleLoginWithGithub}>Login with github</button>
-    </div>
+    <Grid container component="main" className={classes.root}>
+      <CssBaseline />
+      <Grid item xs={false} sm={4} md={7} className={classes.image} />
+      <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+        <div className={classes.paper}>
+          <Avatar className={classes.avatar}>
+            <LockOutlinedIcon />
+          </Avatar>
+          <Typography component="h1" variant="h5">
+            Sign in
+          </Typography>
+			<br/>
+			<br/>
+			<br/>
+			<br/>
+          <div>
+            <AwesomeButtonSocial type="github" onPress={handleLoginWithGithub}>
+              Sign in with Github
+            </AwesomeButtonSocial>
+          </div>
+        </div>
+      </Grid>
+    </Grid>
   );
 };
 
