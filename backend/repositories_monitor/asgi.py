f598fae3ea62da062c0e35addfe257c65e9e4327
Paulo <demelofernandes.paulo@gmail.com>
2019-12-18 21:38:00 -0300

    :wrench: config

--- backend/repositories_monitor/asgi.py
+++ backend/repositories_monitor/asgi.py
@@ -3,7 +3,7 @@ import django
 from channels.routing import get_default_application
 
 os.environ.setdefault(
-    "DJANGO_SETTINGS_MODULE", "repositories_monitor.settings.local_base"
+    "DJANGO_SETTINGS_MODULE", "repositories_monitor.settings.production"
 )
 django.setup()
 application = get_default_application()
